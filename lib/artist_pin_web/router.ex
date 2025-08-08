2e0437b35ee8a41d68a1d25b4d23474609c9d9c6
Son Do <son.do.ext@razer.com>
2025-08-09 03:03:00 +0700

    Adds thermostat live view functionality
    Introduces a new live view for the thermostat feature, allowing users to see and adjust the current temperature.
    
    Removes an outdated link to the Elixir Forum and replaces it with a link to the new thermostat page.
    
    Updates the router to include a route for the thermostat live view.

--- lib/artist_pin_web/router.ex
+++ lib/artist_pin_web/router.ex
@@ -18,6 +18,8 @@ defmodule ArtistPinWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :home
+
+    live "/thermostat", ThermostatLive
   end
 
   # Other scopes may use custom stacks.
